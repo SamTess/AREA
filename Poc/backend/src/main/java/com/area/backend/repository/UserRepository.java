941c377b4b289403112eb30c60f3e66fff136510
ms-tristan <tristan.masse@epitech.eu>
2025-09-22 11:21:07 +0200

    [REFAC](backend): changed file structure

--- Poc/backend/src/main/java/com/area/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.area.backend.repository;
-
-import com.area.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
--- Poc/backend/src/main/resources/db/migration/V3__add_todo_order_column.sql
+++
@@ -1,2 +0,0 @@
--- Add 'todo_order' column to todos table
-ALTER TABLE todos ADD COLUMN todo_order INT;
